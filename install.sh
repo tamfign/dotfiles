c3c2f672cc7b6af06ee2820daa0ac7706a7cba74
Andrew <andrew.a.yang@pwc.com>
2022-02-02 04:57:17 +0000

    Add gcp cli install

--- install.sh
+++ install.sh
@@ -12,3 +12,9 @@ sudo apt-get install -y git-core bash-completion
 # Nodejs
 curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
 sudo apt-get install -y nodejs
+
+# GCP Cli
+sudo apt-get install apt-transport-https ca-certificates gnupg
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+sudo apt-get update && sudo apt-get install google-cloud-sdk
