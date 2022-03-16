bf87aa7304803c552c8a4dbbfe421f920a443117
Andrew <andrew.a.yang@pwc.com>
2022-03-16 05:12:03 +0000

    Add aws and aws cdk

--- install.sh
+++ install.sh
@@ -18,3 +18,11 @@ sudo apt-get install apt-transport-https ca-certificates gnupg
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 sudo apt-get update && sudo apt-get install google-cloud-sdk
+
+# AWS Cli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+# AWS CDK
+sudo npm install -g aws-cdk
